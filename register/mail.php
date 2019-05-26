4a0da74b4231e5bf6f59cddb21ad4d3df588ae5f
Ashish Kumar <ashishkr351@gmail.com>
2019-05-26 09:12:35 +0530

    Minor change

--- register/mail.php
+++ register/mail.php
@@ -12,12 +12,12 @@ $mail->isSMTP();
  * Server Configuration
  */
 
-                $mail->Host = 'smtp.gmail.com'; // Which SMTP server to use.
-                $mail->Port = 587; // Which port to use, 587 is the default port for TLS security.
-                $mail->SMTPSecure = 'tls'; // Which security method to use. TLS is most secure.
-                $mail->SMTPAuth = true; // Whether you need to login. This is almost always required.
-                $mail->Username = ""; // Your Gmail address.
-                $mail->Password = ""; // Your Gmail login password or App Specific Password.
+$mail->Host = 'smtp.gmail.com'; // Which SMTP server to use.
+$mail->Port = 587; // Which port to use, 587 is the default port for TLS security.
+$mail->SMTPSecure = 'tls'; // Which security method to use. TLS is most secure.
+$mail->SMTPAuth = true; // Whether you need to login. This is almost always required.
+$mail->Username = ""; // Your Gmail address.
+$mail->Password = ""; // Your Gmail login password or App Specific Password.
 
 /*
  * Message Configuration
