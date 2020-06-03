661e4f373b868809697c9a6583e9a5c2279f8710
Ashish Kumar <ashishkr23438@gmail.com>
2020-06-03 14:51:33 +0530

    Add files via upload

--- register/db.php
+++ register/db.php
@@ -1,9 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "ashishkr";
-$password = "12345";
-$database = "spardha19";
-$conn = mysqli_connect($servername, $username, $password, $database);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
